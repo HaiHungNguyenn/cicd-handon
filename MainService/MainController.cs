01c59c03a6f3a63249b3a5d56b3d95e3262c9885
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-08-26 08:57:25 +0700

    api 1 and 2

---
+++ MainService/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MainService;
+[Route("/api/[controller]")]
+[ApiController]
+public class MainController : ControllerBase
+{
+    [HttpGet("/hello")]
+    public IActionResult Hello()
+    {
+        return Ok("Hello");
+    }
+    
+    [HttpPost("/goodbye")]
+    public IActionResult GoodBye()
+    {
+        return Ok("Good bye.");
+    }
+}
